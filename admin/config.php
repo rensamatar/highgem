97b0b6546e8103cf08fac3d92d07e75a48280736
Sarawut Popadcha <rensamatar@gmail.com>
2015-02-09 12:54:23 +0700

    first init project

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://highgem.dev/admin/');
+define('HTTP_CATALOG', 'http://highgem.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://highgem.dev/admin/');
+define('HTTPS_CATALOG', 'http://highgem.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/tar/Sites/highgem/admin/');
+define('DIR_SYSTEM', '/Users/tar/Sites/highgem/system/');
+define('DIR_LANGUAGE', '/Users/tar/Sites/highgem/admin/language/');
+define('DIR_TEMPLATE', '/Users/tar/Sites/highgem/admin/view/template/');
+define('DIR_CONFIG', '/Users/tar/Sites/highgem/system/config/');
+define('DIR_IMAGE', '/Users/tar/Sites/highgem/image/');
+define('DIR_CACHE', '/Users/tar/Sites/highgem/system/cache/');
+define('DIR_DOWNLOAD', '/Users/tar/Sites/highgem/system/download/');
+define('DIR_UPLOAD', '/Users/tar/Sites/highgem/system/upload/');
+define('DIR_LOGS', '/Users/tar/Sites/highgem/system/logs/');
+define('DIR_MODIFICATION', '/Users/tar/Sites/highgem/system/modification/');
+define('DIR_CATALOG', '/Users/tar/Sites/highgem/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'highgem');
+define('DB_PREFIX', 'hg_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://highgem.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://highgem.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/tar/Sites/highgem/catalog/');
+define('DIR_SYSTEM', '/Users/tar/Sites/highgem/system/');
+define('DIR_LANGUAGE', '/Users/tar/Sites/highgem/catalog/language/');
+define('DIR_TEMPLATE', '/Users/tar/Sites/highgem/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/tar/Sites/highgem/system/config/');
+define('DIR_IMAGE', '/Users/tar/Sites/highgem/image/');
+define('DIR_CACHE', '/Users/tar/Sites/highgem/system/cache/');
+define('DIR_DOWNLOAD', '/Users/tar/Sites/highgem/system/download/');
+define('DIR_UPLOAD', '/Users/tar/Sites/highgem/system/upload/');
+define('DIR_MODIFICATION', '/Users/tar/Sites/highgem/system/modification/');
+define('DIR_LOGS', '/Users/tar/Sites/highgem/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'highgem');
+define('DB_PREFIX', 'hg_');
